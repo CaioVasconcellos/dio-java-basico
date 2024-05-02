8fc19937d67313afcb796788b26125bd7de667d8
Caio <ceduardorv@outlook.com>
2024-05-02 10:14:50 -0300

    Desafio-ContaBanco

---
+++ ContaBanco/src/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+import service.ContaBancoService;
+
+public class test {
+    public static void main(String[] args) {
+        ContaBancoService contaBancoService = new ContaBancoService();
+        contaBancoService.criarConta();
+    }
+}
